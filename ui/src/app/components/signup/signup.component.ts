cea4335d24e1adc1fa99a7075373b20e23b3a795
Krishnan S <skrish22195@gmail.com>
2023-11-12 12:48:21 +0530

    UI: Add Sign up form and API integration

---
+++ ui/src/app/components/signup/signup.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { ToastService } from 'src/app/services/toast.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router,
+    private toastService: ToastService
+  ) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      this.userService.signUp(this.signupForm.value).subscribe({
+        next: response => {
+          this.router.navigate(['/home']);
+        },
+        error: error => {
+          this.toastService.showError(`Error during Sign up ${error.message}`, 2500);
+        }
+      });
+    } else {
+      this.markFormFieldsAsTouched();
+    }
+  }
+  get form() {
+    return this.signupForm.controls;
+  }
+
+  private markFormFieldsAsTouched() {
+    Object.keys(this.signupForm.controls).forEach(field => {
+      const control = this.signupForm.get(field);
+      control?.markAsTouched({ onlySelf: true });
+    });
+  }
+}
---
+++ ui/src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private API_URL = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  signUp(userDetails: { name: String; password: String; email: String }): Observable<any> {
+    return this.http.post(`${this.API_URL}/user/sign-up`, userDetails);
+  }
+}
