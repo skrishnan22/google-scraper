3aab7948a0925dcd500fa2669caaabe7569ed177
Krishnan S <skrish22195@gmail.com>
2023-11-12 12:27:38 +0530

    UI: Add routing for signup, login and home components

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -8,6 +8,19 @@ import { FileUploadComponent } from './components/file-upload/file-upload.compon
 import { ProgressModalComponent } from './components/progress-modal/progress-modal.component';
 import { NgbModule, NgbActiveModal, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { KeywordTableComponent } from './components/keyword-table/keyword-table.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -15,15 +28,20 @@ import { KeywordTableComponent } from './components/keyword-table/keyword-table.
     NavBarComponent,
     FileUploadComponent,
     ProgressModalComponent,
-    KeywordTableComponent
+    KeywordTableComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    NgbModule,
-    NgbToastModule
+    BrowserModule, 
+    HttpClientModule, 
+    NgbModule, 
+    NgbToastModule, 
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [NgbActiveModal],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
